64741a3af35a7d7aaa6c63bde930a5b8abda03c4
saimaja <saimaakhtar@gmail.com>
2020-04-23 12:47:04 -0400

    saimas edits 1

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,10 @@
 class Cat
   # code goes here
+
+  attr_reader :name, :owner
+  def initalize(name, owner)
+    @name = name
+    @owner = owner
+  end
+
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,11 @@
 class Dog
   # code goes here
+
+  attr_reader :name, :owner
+  def initalize(name, owner)
+    @name = name
+    @owner = owner
+  end
+
+
 end
