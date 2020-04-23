67a08c2318a10e98f5b7d8737aaa336ff4a7b53a
mertoz41 <mertozkaynak@windowslive.com>
2020-04-23 12:53:30 -0400

    first commit

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,43 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  @@all = []
+
+  def initialize(name)
+    @name = name 
+    @species = "human" 
+    @@all << self
+
+  end 
+  
+
+  def say_species
+    "I am a #{self.species}."
+  end 
+  
+
+  def self.all
+    @@all
+  end 
+
+  def self.count
+    @@all.length
+  end 
+
+  def self.reset_all
+    @@all = []
+    #binding.pry 
+    Owner.count 
+  end 
+
+
+
+
+
+
+
 end
+mert = Owner.new("Mert")
+davis = Owner.new("Davis")
+Owner.reset_all
