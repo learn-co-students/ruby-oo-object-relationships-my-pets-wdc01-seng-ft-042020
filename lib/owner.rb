be4ec2481ea5cf5f77c38b5860367ef5ef71c2b2
agdsarge <agdsargeant@gmail.com>
2020-04-23 14:19:44 -0400

    cat and dog rbs finished. did some owner methods.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,43 +1,54 @@
 require 'pry'
 
+require_relative "./cat.rb"
+require_relative "./dog.rb"
+
+
 class Owner
   attr_reader :name, :species
   @@all = []
 
   def initialize(name)
-    @name = name 
-    @species = "human" 
+    @name = name
+    @species = "human"
     @@all << self
-
-  end 
-  
+  end
 
   def say_species
     "I am a #{self.species}."
-  end 
-  
+  end
 
   def self.all
     @@all
-  end 
+  end
 
   def self.count
     @@all.length
-  end 
+  end
 
   def self.reset_all
     @@all = []
-    #binding.pry 
-    Owner.count 
-  end 
+    #binding.pry
+    Owner.count
+  end
 
+  def cats #returns a collection of all the cats that belong to the owner
+      Cat.all.select {|cat| cat.owner == self}
+  end
 
-
-
-
+  def dogs
+      Dog.all.select {|dog| dog.owner == self}
+  end
 
 
 end
-mert = Owner.new("Mert")
-davis = Owner.new("Davis")
-Owner.reset_all
+
+#  mert = Owner.new("Mert")
+# ella = Dog.new("Ella", mert)
+# lady = Dog.new("Lady", mert)
+# davis = Owner.new("Davis")
+# tabby = Dog.new("Tabby", davis)
+#
+# print mert.dogs
+
+# Owner.reset_all
