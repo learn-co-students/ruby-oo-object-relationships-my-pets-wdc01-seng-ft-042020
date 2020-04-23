de32ada08b6b4c9e43242346f3f6db73aad47d12
saimaja <saimaakhtar@gmail.com>
2020-04-23 14:11:14 -0400

    saima's cat edits

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,20 @@
 class Cat
   # code goes here
 
-  attr_reader :name, :owner
-  def initalize(name, owner)
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  @@all = []
+
+  def initialize(name, owner)
     @name = name
     @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
 end
