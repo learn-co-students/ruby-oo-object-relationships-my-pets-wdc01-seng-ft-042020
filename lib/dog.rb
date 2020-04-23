165955f5ebef9353a0eca12fd3952720df8457aa
Alexander_Gautherin <alexander.gautherin@uconn.edu>
2020-04-23 14:43:26 -0400

    29-1

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,19 @@
 class Dog
   # code goes here
+  attr_reader :name
+  attr_accessor :owner, :mood
 
-  attr_reader :name, :owner
-  def initalize(name, owner)
+  @@all = []
+
+  def initialize(name, owner, mood = "nervous")
     @name = name
     @owner = owner
+    @mood = mood
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
 
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,6 @@
+require_relative './cat.rb'
+require_relative './dog.rb'
+
 class Owner
   # code goes here
   attr_reader :name, :species
@@ -23,7 +26,36 @@ class Owner
     def self.reset_all
       self.all.clear
     end
-    
+
+
+
+    def cats
+      Cat.all.select {|cat| cat.owner == self}
+    end
+
+    def dogs
+      Dog.all.select {|dog| dog.owner == self}
+    end
+
+    def buy_cat(name)
+      Cat.new(name, self) 
+    end
+
+    def buy_dog(name)
+      Dog.new(name, self) 
+    end
+
+    def walk_dogs()
+      dogs.each {|dog| dog.mood = "happy"}
+    end
+
+    def feed_cats
+      cats.map {|cat| cat.mood = "happy"}
+    end
+
+    def sell_pets
+
+    end
 end
 
 
